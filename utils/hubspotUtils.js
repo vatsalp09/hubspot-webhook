dc5000006ece7b65081e65baea442d23ea054b5b
vatsal-crescentek <vatsal@outrightsolutions.net>
2025-07-04 17:22:45 +0530

    update hubspot auth scope

--- utils/hubspotUtils.js
+++ utils/hubspotUtils.js
@@ -1,7 +1,7 @@
 function generateInstallUrl(crmUserId) {
   const clientId = process.env.HUBSPOT_CLIENT_ID;
   const redirectUri = encodeURIComponent(process.env.HUBSPOT_REDIRECT_URI);
-  const scopes = encodeURIComponent('crm.objects.contacts.read crm.objects.contacts.write oauth');
+  const scopes = encodeURIComponent('crm.objects.contacts.read crm.objects.contacts.write crm.schemas.contacts.read crm.schemas.contacts.write oauth');
 
   return `https://app.hubspot.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes}&state=${crmUserId}`;
 }
